b1f486cf8079e339634b6e7c66a148b356bb4eb8
Irshad <irsahmd07@gmail.com>
2023-12-04 23:00:09 +0530

    My First Commit c1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from './components/header/header';
+import Home from './pages/home/home';
+import MovieList from './components/MovieList/movieList';
+import Movie from './pages/home/movieDetails/movie';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+          <Routes>
+            <Route index element={<Home />}></Route>
+            <Route path='movie/:id' element={<h1>{<Movie/>}</h1>}></Route>
+            <Route path='movies/:type' element={<h1>{<MovieList />}</h1>}></Route>
+            <Route path='/*' element={<h1>Error page</h1>}></Route>
+          </Routes>
+      </Router>
     </div>
   );
 }
